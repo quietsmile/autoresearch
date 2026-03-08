258723303f43867b3221d2e6d90b565d891cf0a3
dengyang <dengyang@xiaohongshu.com>
2026-03-08 16:12:41 +0000

    UNEMBEDDING_LR 0.004->0.008

--- train.py
+++ train.py
@@ -436,7 +436,7 @@ WINDOW_PATTERN = "SSSL" # sliding window pattern: L=full, S=half context
 # Optimization
 TOTAL_BATCH_SIZE = 2**18 # ~262K tokens per optimizer step (2x steps for deeper model)
 EMBEDDING_LR = 0.6      # learning rate for token embeddings (Adam)
-UNEMBEDDING_LR = 0.004  # learning rate for lm_head (Adam)
+UNEMBEDDING_LR = 0.008  # learning rate for lm_head (Adam)
 MATRIX_LR = 0.04        # learning rate for matrix parameters (Muon)
 SCALAR_LR = 0.5         # learning rate for per-layer scalars (Adam)
 WEIGHT_DECAY = 0.1      # cautious weight decay for Muon
