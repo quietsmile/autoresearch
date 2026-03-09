b6cb7a014ac3e287033b12f6fb4a8ada3a61adc7
dengyang <dengyang@xiaohongshu.com>
2026-03-09 00:23:20 +0000

    experiment: medium_window=//2=1024 (GPU4 found M=1024 better than M=512)

--- train.py
+++ train.py
@@ -196,7 +196,7 @@ class GPT(nn.Module):
         assert all(c in "SML" for c in pattern)
         long_window = config.sequence_len
         short_window = long_window // 16
-        medium_window = long_window // 4
+        medium_window = long_window // 2
         char_to_window = {"L": (long_window, 0), "S": (short_window, 0), "M": (medium_window, 0)}
         window_sizes = []
         for layer_idx in range(config.n_layer):
